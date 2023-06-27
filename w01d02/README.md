db9c78cf3e60978000b5534423b7d537ada2ecfb
Andy Lindsay <andydlindsay@gmail.com>
2023-06-27 12:08:38 -0700

    adds the functionality to add up an array of numbers

--- w01d02/README.md
+++ w01d02/README.md
@@ -1,21 +1,37 @@
 # W01D02 - The Dev Workflow
 
 ### To Do
-- [ ] Curriculum overview
-- [ ] Example problem workflow
-- [ ] Problem solving tips
+- [x] Curriculum overview
+- [x] Example problem workflow
+- [x] Problem solving tips
 - [ ] Review git workflow
 
+* Weeks 1 && 2: fundamentals (FOCAL), tcp, async Lotide clone of Lodash
+* Week 3: backend TinyApp
+* Week 4: frontend Tweeter
+* Week 5: data persistence SQL LightBnB
+* Week 6: midterms (W6D5)
+
+* Weeks 7 && 8: React PhotoLabs
+* Weeks 9 && 10: Rails and Ruby (on the job training)
+* Weeks 11 && 12: Final projects (W12D4)
+
+### During Lecture
+* Please don't take notes
+* Discussion to facilitate deeper learning
+* Instructors will make mistakes
+
+### Problem
+Write a program that takes in an unlimited number of command line arguments and prints out the sum of them. If any argument is not a whole number, skip it. Do not support negative numbers.
+
+1. use pseudocode (figure out the logic)
+
+function sayHello () {}
+const sayHello = function () {};
 
 
-
-
-
-
-
-
-
-
+/lighthouse/week-one/day-two
+/lighthouse/week-two
 
 
 
---
+++ w01d02/num-sum.js
@@ -0,0 +1,70 @@
+// Write a program that takes in an unlimited number of command line arguments and prints out the sum of them. If any argument is not a whole number, skip it. Do not support negative numbers.
+
+// 1. use pseudocode (figure out the logic)
+
+// 1. take in command line arguments
+
+// console.log(process.argv);
+
+// remove the first two elements from the process.argv array
+
+// process.argv.shift(); // remove the first element
+// process.argv.shift(); // remove the first element
+// process.argv.splice(0, 2);
+
+const numArray = process.argv.slice(2);
+// const numArray = ['5', '10', '15', '3.14', '-100'];
+
+// console.log(process.argv);
+console.log(numArray);
+
+// 2. sum the command line arguments together
+
+// loop through the provided array
+// c-style, for..in, for..of
+
+// c-style
+// for (let i = 2; i < numArray.length; i += 1) {
+//   const element = numArray[i];
+//   console.log(element);
+// }
+
+// console.log();
+
+// // for..in
+// for (const index in numArray) {
+//   const element = numArray[index];
+//   console.log(element);
+// }
+
+// console.log();
+
+// for..of
+// for (const element of numArray) {
+//   console.log(element);
+// }
+
+
+// loop through the provided array
+let total = 0;
+
+for (const element of numArray) {
+  // total += element;
+  // total = total + element;
+
+  const num = Number(element);
+
+  // if (Number.isInteger(num) === true) {
+  //   if (num > 0) {
+  //     total = total + num;
+  //   }
+  // }
+
+  // if (Number.isInteger(num) === true && num > 0) {
+  if (Number.isInteger(num) && num > 0) {
+    total = total + num;
+  }
+}
+
+console.log('total', total);
+
